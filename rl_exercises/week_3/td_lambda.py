8db5537a4478ee4769e6d862d19aacd2d0c41d47
Moritz Baron <moritz.baron@protonmail.com>
2026-05-02 14:26:28 +0200

    w3-ex3: Implemented init of TDLambda agent

--- rl_exercises/week_3/td_lambda.py
+++ rl_exercises/week_3/td_lambda.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, DefaultDict
+
+from collections import defaultdict
 
 import gymnasium as gym
 import numpy as np
 from rl_exercises.agent import AbstractAgent
+from rl_exercises.week_3 import EpsilonGreedyPolicy
 
 State = Any
 
@@ -15,9 +18,10 @@ class TDLambda(AbstractAgent):
     def __init__(
         self,
         env: gym.Env,
+        policy: EpsilonGreedyPolicy,
         alpha: float = 0.5,  # Learning rate
         gamma: float = 1.0,  # Discount factor
-        lamda: float = 0.5,  # Trace decay
+        lamd: float = 0.5,  # Trace decay
     ) -> None:
         """Initialize the TD agent
 
@@ -29,28 +33,31 @@ class TDLambda(AbstractAgent):
             Learning Rate, by default 0.5
         gamma : float, optional
             Discount Factor , by default 1.0
-        lamda : float, optional
+        lamd : float, optional
             Trace decay, by default 0.5
         """
         # Check hyperparameter boundaries
         assert 0 <= gamma <= 1, "Gamma should be in [0, 1]"
         assert alpha > 0, "Learning rate has to be greater than 0"
-        assert 0 <= lamda <= 1, "Lamda should be in [0, 1]"
+        assert 0 <= lamd <= 1, "Lamda should be in [0, 1]"
 
         self.env = env
         self.gamma = gamma
         self.alpha = alpha
-        self.lambda_factor = lamda
+        self.lambd = lamd
 
-        # # number of actions → used by Q’s default factory
-        # self.n_actions = env.action_space.n
+        # number of actions → used by Q’s default factory
+        self.n_actions = env.action_space.n
 
-        # # Build Q so that unseen states map to zero‐vectors
-        # self.Q: DefaultDict[Any, np.ndarray] = defaultdict(
-        #     lambda: np.zeros(self.n_actions, dtype=float)
-        # )
+        # Build Q so that unseen states map to zero‐vectors
+        self.Q: DefaultDict[Any, np.ndarray] = defaultdict(
+            lambda: np.zeros(self.n_actions, dtype=float)
+        )
 
-        # self.policy = policy
+        # Eligibility traces as on page 16 of the paper (PDF page 8)
+        self.e = defaultdict(lambda: np.zeros(self.n_actions))
+
+        self.policy = policy
 
     def predict_action(
         self, state: np.array, info: dict = {}, evaluate: bool = False
